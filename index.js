19de6b89b7830b6dc5b387141b2366fb3b016758
Alexei Okhapkin <alexeyyago@gmail.com>
2023-12-06 23:07:02 +0300

    EventEmitter

--- index.js
+++ index.js
@@ -6,20 +6,41 @@ class EventEmitter {
   on(eventName, callback) {
       // code here
       if (!this.events[eventName]) {
-          
+        this.events[eventName] = [];
+        this.events[eventName].push(callback);
+      } else {
+        this.events[eventName].push(callback);
       }
   }
 
   off(eventName, callback) {
       // code here
+      if (this.events[eventName]) {
+        this.events[eventName] = this.events[eventName].filter(item => item !== callback);
+      }
   }
 
   once(eventName, callback) {
       // code here
+      const onceF = (fn) => {
+        const f = (arg) => {
+          this.off(eventName, f);
+          return fn(arg);
+        };
+  
+        return f;
+      };
+
+      this.on(eventName, onceF(callback));
   }
 
   emit(eventName, args) {
       // code here
+      if (this.events[eventName]) {
+        this.events[eventName].forEach(event => {
+          event.call(null, args);
+        });
+      }
   }
 }
 
