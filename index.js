127bb2e74b40ed569fe20c528a3fc23710567928
Alexei Okhapkin <alexeyyago@gmail.com>
2023-12-06 23:11:51 +0300

    BroadcastEventEmitter

--- index.js
+++ index.js
@@ -35,17 +35,28 @@ class EventEmitter {
   }
 
   emit(eventName, args) {
-      // code here
-      if (this.events[eventName]) {
-        this.events[eventName].forEach(event => {
-          event.call(null, args);
-        });
+    if (this.events[eventName]) {
+      for (let i = 0; i < this.events[eventName].length; i++) {
+        this.events[eventName][i].call(null, args);
       }
+    }
   }
 }
 
 class BroadcastEventEmitter extends EventEmitter {
-  emit(eventName, args) {
-      // code here
+  emit(eventName, ...args) {
+    if (eventName !== '*') {
+      if (this.events[eventName]) {
+        for (let i = 0; i < this.events[eventName].length; i++) {
+          this.events[eventName][i].apply(null, args);
+        }
+      }
+    } else {
+      for (let key in this.events) {
+        for (let i = 0; i < this.events[key].length; i++) {
+          this.events[key][i].apply(null, args);
+        }
+      }
+    }
   }
 }
